679ca531afa9ac1b8aba1446a96a367787d9670a
salehahmed325 <salehahmed904@gmail.com>
2024-09-30 18:15:09 +0600

    added comments of the js functions

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,11 +1,22 @@
+//function for the game
 function gameLoop() {
+    //get the random alphabet
     const alphabet = getARandomAlphabet();
-    const showAlphabet = document.getElementById('alphabet');
+
+    //show the random alphabet
+    const showAlphabet = document.getElementById('alphabet-to-show');
     showAlphabet.innerText = alphabet;
 }
 
+//function for the home screen play now button
 function playNow() {
+
+    //hide the home screen
     hideElementById('home');
+
+    //show the game screen
     showElementById('play-ground');
+
+    //start the game loop
     gameLoop();
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,21 +1,26 @@
+//hide the home screen
 function hideElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('hidden');
 }
 
+//show the playground screen
 function showElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
 
+//generate random alphabet for playground
 function getARandomAlphabet(){
 
     //create a array of all 16 alphabets
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetString.split('');
 
-    //generate random numbers from 0-25
+    //generate random number from 0-25
     const randomNumber = Math.random() * 25;
     const randomIndex = Math.round(randomNumber);
+
+    //return the random number as the index of the alphabets array
     return alphabets[randomIndex];
 }
