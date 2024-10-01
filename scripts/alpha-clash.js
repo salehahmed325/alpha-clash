dc4edee890f4aaac1a30d6dc29e6949b202785e0
salehahmed325 <salehahmed904@gmail.com>
2024-10-01 16:18:35 +0600

    Update and show score

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -3,15 +3,26 @@ function keyupEvent(event) {
     const keyPressed = event.key;
     const keyToPress = document.getElementById('alphabet-to-show');
     const keyToBePressed = keyToPress.innerText;
-
     //point and life calculation
     if (keyPressed == keyToBePressed) {
-        console.log("point");
+        //current score
+        const score = document.getElementById('score');
+        const scoreValue = parseInt(score.innerText);
+        const newScore = scoreValue + 1;
+        //show current score
+        score.innerText = newScore;
+        //start second round
         removeHighlightAlphabets(keyToBePressed);
         gameLoop();
     }
     else {
         console.log("no point");
+        //current life
+        const life = document.getElementById('life');
+        const lifeValue = parseInt(life.innerText);
+        const newLife = lifeValue - 1;
+        //show current life
+        life.innerText = newLife;
     }
 }
 
@@ -22,24 +33,19 @@ document.addEventListener('keyup', keyupEvent);
 function gameLoop() {
     //get the random alphabet
     const alphabet = getARandomAlphabet();
-
     //show the random alphabet
     const showAlphabet = document.getElementById('alphabet-to-show');
     showAlphabet.innerText = alphabet;
-
     //highlight the random alphabet
     highlightAlphabets(alphabet);
 }
 
 //function for the home screen play now button
 function start() {
-
     //hide the home screen
     hideElementById('home');
-
     //show the game screen
     showElementById('play-ground');
-
     //start the game loop
     gameLoop();
 }
