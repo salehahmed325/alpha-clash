4d9de66afa548681eec6ede61e32888bdc5ce3e0
salehahmed325 <salehahmed904@gmail.com>
2024-10-02 12:52:11 +0600

    Code clarity with minor changes

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,77 +1,83 @@
-//keyboard event function
+//Keyboard event function
 function keyupEvent(event) {
     const keyPressed = event.key;
     const keyToPress = document.getElementById('alphabet-to-show');
     const keyToBePressed = keyToPress.innerText;
-    //point and life calculation
+    
+    //Point and life calculation
     if (keyPressed == keyToBePressed) {
-        //current score
+        //Current score
         const score = getElementsValueById('score');
-        //increase score
+        //Increase score
         const newScore = score + 1;
-        //show new score
+        //Show new score
         setElementsValueById('score', newScore);
-        //start second round
+
+        //Start second round
+        //Remove previous highlight
         removeHighlightAlphabets(keyToBePressed);
+        //Continue game
         gameLoop();
     }
     else {
-        //current life
+        //Current life
         const life = getElementsValueById('life');
-        //decrease life
+        //Decrease life
         const newLife = life - 1;
-        //show current life
+        //Show current life
         setElementsValueById('life', newLife);
+        
+        //If life is zero, game over
         if (newLife === 0) {
-            //show the final score screen
+            //Show the final score screen
             gameOver();
         }
     }
 
-    //press Esc to stop the game
+    //Press Esc to stop the game
     if (keyPressed == 'Escape') {
         gameOver();
     }
 }
 
-//keyboard event
+//Keyboard event
 document.addEventListener('keyup', keyupEvent);
 
-//function for the game
+//Function for the game
 function gameLoop() {
-    //get the random alphabet
+    //Get the random alphabet
     const alphabet = getARandomAlphabet();
-    //show the random alphabet
+    //Show the random alphabet
     setElementsValueById('alphabet-to-show', alphabet);
-    //highlight the random alphabet
+    //Highlight the random alphabet
     highlightAlphabets(alphabet);
 }
 
-//function for the home screen play now button
+//Function for the home screen play now button
 function start() {
-    //hide the home screen
+    //Hide the home screen
     hideElementById('home');
-    //hide the final score screen
+    //Hide the final score screen
     hideElementById('final-score');
-    //show the game screen
+    //Show the game screen
     showElementById('play-ground');
-    //reset life and score
+    //Reset life and score
     setElementsValueById('life', 5);
     setElementsValueById('score', 0);
-    //start the game loop
+    //Start the game loop
     gameLoop();
 }
 
-//game over function
+//Game over function
 function gameOver() {
-    //hide the playground  screen
+    //Hide the playground  screen
     hideElementById('play-ground');
-    //show the game over screen
+    //Show the game over screen
     showElementById('final-score');
-    //show the total score
+    //Show the total score
     const totalScore = getElementsValueById('score')
     setElementsValueById('total-score', totalScore);
-    //remove the last highlight from alphabet
+    //Remove the last highlight from alphabet
     const lastAlphabet = getElementTextById('alphabet-to-show');
     removeHighlightAlphabets(lastAlphabet);
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,58 +1,55 @@
-//hide the screen
+//Hide the screen
 function hideElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('hidden');
 }
 
-//show the screen
+//Show the screen
 function showElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
 
-//get the elements value
+//Get the elements value
 function getElementsValueById(elementId) {
     const element = document.getElementById(elementId);
     const elementValue = parseInt(element.innerText);
     return elementValue;
 }
 
-//set the elements value
+//Set the elements value
 function setElementsValueById(elementId, value) {
     const element = document.getElementById(elementId);
     element.innerText = value;
 }
 
-//get element text
-function getElementTextById(elementId){
+//Get element text
+function getElementTextById(elementId) {
     const element = document.getElementById(elementId);
     const elementText = element.innerText;
     return elementText;
 }
 
-//highlight the random alphabet
-function highlightAlphabets(elementId){
+//Highlight the random alphabet
+function highlightAlphabets(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('highlight-alphabet');
 }
 
-//remove the highlight from alphabet
-function removeHighlightAlphabets(elementId){
+//Remove the highlight from alphabet
+function removeHighlightAlphabets(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('highlight-alphabet');
 }
 
-//generate random alphabet for playground
-function getARandomAlphabet(){
-
-    //create a array of all 16 alphabets
+//Generate random alphabet for playground
+function getARandomAlphabet() {
+    //Create a array of all 16 alphabets
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz/';
     const alphabets = alphabetString.split('');
-
-    //generate random number from 0-25
+    //Generate random number from 0-25
     const randomNumber = Math.random() * 26;
     const randomIndex = Math.round(randomNumber);
-
-    //return the random number as the index of the alphabets array
+    //Return the random number as the index of the alphabets array
     return alphabets[randomIndex];
 }
